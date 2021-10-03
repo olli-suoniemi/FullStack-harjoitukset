3b6cfb3d165992da25a921f6a637e27fac98ba04
olli-suoniemi <74099614+olli-suoniemi@users.noreply.github.com>
2021-10-03 11:52:55 +0300

    Delete osa3/3.11_puhelinluettelo_fullstack directory

--- osa3/3.11_puhelinluettelo_fullstack/build/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = (props) => {
-    return(
-        <div>filter shown with <input onChange={props.handleFiltering}/></div>
-    );
-};
-
-export default Filter
